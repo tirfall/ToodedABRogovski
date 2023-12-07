02b2804228cf0319e30626d9c6d71f941821fe63
opilane <opilane@HP-CZC2349HTY>
2023-12-07 10:00:40 +0200

    update

--- Pood.cs
+++ Pood.cs
@@ -12,7 +12,9 @@ namespace ToodedAB
     public partial class Pood : Form
     {
         Label title;
-        Button start;
+        Button start, setting, exit;
+        Settings settings;
+        Main main;
         public Pood()
         {
             this.Width = 1200;
@@ -26,8 +28,71 @@ namespace ToodedAB
             start = new Button() { BackgroundImage = Properties.Resources.cart, Size = new Size(600,70), BackgroundImageLayout = ImageLayout.Zoom };
             start.Location = new Point((this.Width - start.Width) / 2-20, title.Bottom+start.Height+200);
             start.MouseHover += Start_MouseHover;
+            setting = new Button() { BackgroundImage = Properties.Resources.set, Size = new Size(600, 70), BackgroundImageLayout = ImageLayout.Zoom };
+            setting.Location = new Point((this.Width - start.Width) / 2-20, start.Bottom+title.Height+50);
+            setting.MouseHover +=Setting_MouseHover;
+            exit = new Button() { Text = "EXIT", Size = new Size(600, 70), Font = new Font("Arial",30, FontStyle.Bold) };
+            exit.Location = new Point((this.Width - start.Width) / 2-20, setting.Bottom+title.Height+50);
+            exit.MouseHover+=Exit_MouseHover;
 
-            Controls.AddRange(new Control[] { title, start });
+            exit.MouseClick+=Exit_MouseClick;
+            start.MouseClick+=Start_MouseClick;
+            setting.MouseClick+=Setting_MouseClick;
+
+            Controls.AddRange(new Control[] { title, start, setting, exit });
+        }
+
+        private void Setting_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            settings = new Settings();
+            settings.Closed += (s, args) => this.Close();
+            settings.Show();
+        }
+
+        private void Start_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            main = new Main();
+            main.Closed += (s, args) => this.Close();
+            main.Show();
+        }
+
+        private void Exit_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        private async void Exit_MouseHover(object sender, EventArgs e)
+        {
+            do
+            {
+                exit.Text = "";
+                foreach (string item in new string[] { " E ", " X ", " I ", " T " })
+                {
+                    exit.Text += item;
+                    if (!exit.ClientRectangle.Contains(exit.PointToClient(Cursor.Position)))
+                        break;
+                    await Task.Delay(500);
+                    
+                }
+            } while (exit.ClientRectangle.Contains(exit.PointToClient(Cursor.Position)));
+            exit.Text = "EXIT";
+        }
+
+        private async void Setting_MouseHover(object sender, EventArgs e)
+        {
+            do
+            {
+                foreach (Image item in new Image[] { Properties.Resources.set1, Properties.Resources.set2, Properties.Resources.set3})
+                {
+                    Bitmap bmp = new Bitmap(item);
+                    bmp.MakeTransparent(Color.White);
+                    setting.BackgroundImage = bmp;
+                    await Task.Delay(40);
+                }
+            } while (setting.ClientRectangle.Contains(setting.PointToClient(Cursor.Position)));
+            setting.BackgroundImage = Properties.Resources.set;
         }
 
         private async void Start_MouseHover(object sender, EventArgs e)
---
+++ Settings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToodedAB.Properties;
+
+namespace ToodedAB
+{
+    public partial class Settings : Form
+    {
+        Label home;
+        public Settings()
+        {
+            this.Width = 1200;
+            this.Height = 900;
+            this.Text = "VS Pood | Vihane Sipelgas";
+            this.BackgroundImage = Properties.Resources.bg;
+            this.Icon = Properties.Resources.Icon;
+
+            Bitmap bmp = new Bitmap(Properties.Resources.house, 200, 200);
+            home = new Label() { BorderStyle = BorderStyle.Fixed3D, Image = bmp, Size = new Size(200,200), BackColor = Color.Transparent, Location = new Point(Width/2-100,20)};
+            home.MouseHover+=Home_MouseHover;
+            home.MouseLeave+=Home_MouseLeave;
+
+            Controls.AddRange(new Control[] { home });
+
+        }
+
+        private void Home_MouseLeave(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(Properties.Resources.house, 200, 200);
+            home.Image = bmp;
+        }
+
+        private void Home_MouseHover(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(Properties.Resources.house1, 200, 200);
+            home.Image = bmp;
+        }
+    }
+}
